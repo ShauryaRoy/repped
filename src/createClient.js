321d717dc5c60f7fe98b972cd4351a4515e77344
Rithvik Raj <79125456+Rtvk-001@users.noreply.github.com>
2024-08-21 11:28:04 +0530

    Delete src directory

--- src/createClient.js
+++
@@ -1,8 +0,0 @@
-import {createClient} from "@supabase/supabase-js";
-
-export const supabase = createClient
-(
-    "https://ledyfhcgmrilujszjwbg.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlZHlmaGNnbXJpbHVqc3pqd2JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIzNjU3MjUsImV4cCI6MjAzNzk0MTcyNX0.DNufc_Hx0ZZdNkLCSF3o-hL3CUWk56HkdzdZnBBxvEY"  
-
-);
--- src/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import cartReducer from './cartReducer'; // Assume you have some reducers
-
-const store = createStore(cartReducer);
-
-export default store;
